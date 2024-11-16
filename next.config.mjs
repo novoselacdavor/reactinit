fa7525c36305ddf073315f0e295d9ee7e1f1a272
DavorM <davor.maticevic@myzone.com>
2024-11-16 12:07:38 +0100

    feat(src): modifies to src folder

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        port: "",
+        pathname: "/products/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
