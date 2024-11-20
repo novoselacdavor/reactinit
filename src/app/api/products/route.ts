c4e248714a40ac76571ee59ae9b73e286b664fe4
mordekaiser666 <novoselacdavor@gmail.com>
2024-11-20 16:12:32 +0100

    Fetch test json data

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,14 +7,10 @@ export async function GET(request: Request) {
   //   const { searchParams } = new URL(request.url)
   //   const id = searchParams.get('id')
 
-  //   const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'API-Key': process.env.DATA_API_KEY!,
-  //     },
-  //   })
-  //   const product = await res.json()
-  return Response.json({ product: "valja" });
+  const res = await fetch(`https://dummyjson.com/products`);
+  const products = await res.json();
+
+  return Promise.resolve({ products });
 }
 
 export async function POST(request: Request) {}
